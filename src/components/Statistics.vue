9b257f4a4512b3ff17864890c465c7440b451198
alimovshaxzod89 <alimovshaxzod89@gmail.com>
2022-06-09 11:37:14 +0500

    Stats edded

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,10 +1,49 @@
 <template>
-    <div class="statistics container text-center">
+    <div class="statistics text-center">
         <h3>Learning Center Statistics</h3>
+       <div class="stats w-75 d-flex justify-content-between text-center mt-5">
+
+            <div class="stats_group">
+                <div class="my_icon_group">
+                    <box-icon class="my_icon" name='male-female' color='white'></box-icon>
+                </div>
+                <VueJsCounter style="font-size: 25px; line-height: 60px;" start="0" end="34" duration="1500" thousand="." decimal=","></VueJsCounter>
+                <p>Umumiy o'qituvchilar soni</p>
+            </div>
+
+            <div class="stats_group">
+                <div class="my_icon_group">
+                    <box-icon class="my_icon" type='solid' name='group' color='white'></box-icon>
+                </div>
+                <VueJsCounter style="font-size: 25px; line-height: 60px;" start="0" end="156" duration="1500" thousand="." decimal=","></VueJsCounter>
+                <p>Umumiy o'quvchilar soni</p>
+            </div>
+
+            <div class="stats_group">
+                <div class="my_icon_group">
+                    <box-icon class="my_icon" type='solid' name='user-detail' color='white'></box-icon>
+                </div>
+                <VueJsCounter style="font-size: 25px; line-height: 60px;" start="0" end="200" duration="1500" thousand="." decimal=","></VueJsCounter>
+                <p>Tugatgan talabalar soni</p>
+            </div>
+
+            <div class="stats_group">
+                <div class="my_icon_group">
+                    <box-icon class="my_icon" type='solid' name='user-check' color='white'></box-icon>
+                </div>
+                <VueJsCounter style="font-size: 25px; line-height: 60px;" start="0" end="10" duration="1500" thousand="." decimal=","></VueJsCounter>
+                <p>Guruhlar soni</p>
+            </div>
+       </div>
     </div>
 </template>
 <script>
+import 'boxicons'
+import VueJsCounter from 'vue-js-counter'
 export default {
+    components:{
+        VueJsCounter
+    }
     
 }
 </script>
@@ -17,4 +56,32 @@ export default {
     letter-spacing: 0.2px;
     color: #1E1F4B;
 }
+.statistics{
+    display: flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    align-items: center;
+    background-color: rgba(112, 111, 229, 0.02);
+}
+.my_icon_group{
+    width: 70px;
+    height: 70px;
+    background-color: #706FE5;
+    color: white;
+    border-radius: 50%;
+    position: relative;
+    margin: auto;
+}
+.my_icon_group .my_icon{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+.stats_group p{
+    color: #1E1F4B;
+    font-size: 20px;
+    line-height: 15px;
+}
+
 </style>
