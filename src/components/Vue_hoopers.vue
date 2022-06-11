0883cc66c32ed076ac08123a4c130dd4076c330c
alimovshaxzod89 <alimovshaxzod89@gmail.com>
2022-06-11 15:40:06 +0500

    Vue hooper changed

--- src/components/Vue_hoopers.vue
+++ src/components/Vue_hoopers.vue
@@ -5,30 +5,154 @@
     </div>
     <div class="row" >
       <div class="col-md-12" >
-        <hooper :progress="true" :autoPlay="true" :playSpeed="3000" style="height: 450px;">
-          <slide >
-            <img class="w-50" src="@/assets/img/lCenter1.jpg" alt="">
-            <img class="w-50" src="@/assets/img/lCenter1.jpg" alt="">
+        <hooper :progress="true" :autoPlay="false" :playSpeed="3000" style="height: 450px;">
+          <!-- Slide -->
+          <slide class="d-flex justify-content-center">
+
+            <div class="card" style="width: 400px; height: 400px; ">
+              <img class="card-img-top" src="@/assets/img/lCenter1.jpg" alt="Card image cap">
+              <div class="card-body ">
+                <h5 class="card-title">Card title</h5>
+                <p  class="card-text overflow-scroll">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis, quis blanditiis maxime laboriosam sit culpa fuga ipsa corporis? Doloremque vero rerum cumque at minima modi quam quasi molestias perferendis consequuntur, nesciunt officiis libero fuga repellat, tenetur voluptates ipsum sequi! Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
           </slide>
-          <slide>
-            <img class="w-50" src="@/assets/img/lCenter2.jpg" alt="">
-            <img class="w-50" src="@/assets/img/lCenter2.jpg" alt="">
+          <!-- Slide -->
+          <slide class="d-flex justify-content-center">
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter2.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter2.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter2.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
           </slide>
-          <slide>
-            <img class="w-50" src="@/assets/img/lCenter3.jpg" alt="">
-            <img class="w-50" src="@/assets/img/lCenter3.jpg" alt="">
+
+          <!-- Slide -->
+          <slide class="d-flex justify-content-center">
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter3.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter3.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter3.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
           </slide>
-          <slide>
-            <img class="w-50" src="@/assets/img/lCenter4.jpg" alt="">
-            <img class="w-50" src="@/assets/img/lCenter4.jpg" alt="">
+
+          <!-- Slide -->
+          <slide class="d-flex justify-content-center">
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter4.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter4.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter4.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
           </slide>
-          <slide>
-            <img class="w-50" src="@/assets/img/lCenter5.jpg" alt="">
-            <img class="w-50" src="@/assets/img/lCenter5.jpg" alt="">
+          <!-- Slide -->
+          <slide class="d-flex justify-content-center">
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter5.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter5.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter5.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
           </slide>
-          <slide>
-            <img class="w-50" src="@/assets/img/lCenter6.jpg" alt="">
-            <img class="w-50" src="@/assets/img/lCenter6.jpg" alt="">
+          <slide class="d-flex justify-content-center">
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter6.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter6.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 400px; height: 400px">
+              <img class="card-img-top" src="@/assets/img/lCenter6.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
+              </div>
+            </div>
           </slide>
         </hooper>
       </div>
@@ -67,5 +191,9 @@ h3{
   font-weight: 700;
   line-height: 52px;
   letter-spacing: 0.2px;
+}
+.card
+{
+  overflow: scroll !important;
 }
 </style>
